47654b9363e3081fc1c2efb12b2c587f038342c5
Jaskier <m_l@op.pl>
2012-01-06 15:11:22 +0100

    Final 4.1

--- Help/MakeHelp.bat
+++ Help/MakeHelp.bat
@@ -11,21 +11,21 @@ REM -- First make map file from Microsoft Visual C++ generated resource.h
 echo // MAKEHELP.BAT generated Help Map file.  Used by Atari800Win.HPJ. >"Atari800Win.hm"
 echo. >>"Atari800Win.hm"
 echo // Commands (ID_* and IDM_*) >>"Atari800Win.hm"
-makehm ID_,HID_,0x10000 IDM_,HIDM_,0x10000 resource.h >>"Atari800Win.hm"
+makehm ID_,HID_,0x10000 IDM_,HIDM_,0x10000 ..\Res\resource.h >>"Atari800Win.hm"
 echo. >>"Atari800Win.hm"
 echo // Prompts (IDP_*) >>"Atari800Win.hm"
-makehm IDP_,HIDP_,0x30000 resource.h >>"Atari800Win.hm"
+makehm IDP_,HIDP_,0x30000 ..\Res\resource.h >>"Atari800Win.hm"
 echo. >>"Atari800Win.hm"
 echo // Resources (IDR_*) >>"Atari800Win.hm"
-makehm IDR_,HIDR_,0x20000 resource.h >>"Atari800Win.hm"
+makehm IDR_,HIDR_,0x20000 ..\Res\resource.h >>"Atari800Win.hm"
 echo. >>"Atari800Win.hm"
 echo // Dialogs (IDD_*) >>"Atari800Win.hm"
-makehm IDD_,HIDD_,0x20000 resource.h >>"Atari800Win.hm"
+makehm IDD_,HIDD_,0x20000 ..\Res\resource.h >>"Atari800Win.hm"
 echo. >>"Atari800Win.hm"
 echo // Frame Controls (IDW_*) >>"Atari800Win.hm"
-makehm IDW_,HIDW_,0x50000 resource.h >>"Atari800Win.hm"
+makehm IDW_,HIDW_,0x50000 ..\Res\resource.h >>"Atari800Win.hm"
 echo // Controls (IDC_*) >>"Atari800Win.hm"
-makehm IDC_,HIDC_,0x50000 resource.h >>"Atari800Win.hm"
+makehm IDC_,HIDC_,0x50000 ..\Res\resource.h >>"Atari800Win.hm"
 REM -- Make help for Project Atari800Win PLus
 
 
@@ -35,8 +35,8 @@ if errorlevel 1 goto :Error
 if not exist "Atari800Win.hlp" goto :Error
 if not exist "Atari800Win.cnt" goto :Error
 echo.
-copy "Atari800Win.hlp" "..\Distribution\Atari800WinPLus"
-copy "Atari800Win.cnt" "..\Distribution\Atari800WinPLus"
+copy "Atari800Win.hlp" "..\Distribution\"
+copy "Atari800Win.cnt" "..\Distribution\"
 echo.
 goto :done
 
